dcfd4d6cdb98201761b4a58e78df79d48d8338a3
Jesdakorn (Ton) <i@ton.in.th>
2015-06-08 20:08:04 +0900

    initalizer

--- integer_translator.rb
+++ integer_translator.rb
@@ -1,2 +1,7 @@
 class IntegerTranslator
+  attr_reader :input, :translation
+  def initialize(input)
+    raise 'Sorry, only integer can be accepted' unless input.is_a?(Integer)
+    @input = input
+  end
 end
--- spec/integer_translator_spec.rb
+++ spec/integer_translator_spec.rb
@@ -1,4 +1,20 @@
 require './integer_translator'
 
 RSpec.describe IntegerTranslator do
+  describe '#initialize' do
+    context 'with incorrect input' do
+      it 'should initialize input value' do
+        expect { IntegerTranslator.new(20.430) }.to raise_error
+      end
+    end
+
+    context 'with correct input' do
+      it 'should initialize input value' do
+        translator = IntegerTranslator.new(20)
+        expect(translator.input).to eq(20)
+      end
+    end
+  end
+
+  
 end
